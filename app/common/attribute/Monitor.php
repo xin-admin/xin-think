09757607ca61693713f45a72c1c493df8e398aa8
liu <2302563948@qq.com>
2024-09-13 15:09:14 +0800

    feat: 系统监控登录监控

--- app/common/attribute/Monitor.php
+++ app/common/attribute/Monitor.php
@@ -15,24 +15,55 @@ use Attribute;
 use think\facade\Request;
 
 /**
- * 请求注解类
+ * 系统监控注解类
  */
 #[Attribute(Attribute::TARGET_METHOD)]
 class Monitor
 {
 
-    public function __construct(string $name = '')
+    /**
+     * 系统监控注解
+     * @param string $name 接口名称
+     * @param bool $auth 记录权限
+     * @param string $user_id 用户ID
+     */
+    public function __construct(string $name = '', bool $auth = true, string $user_id = '')
     {
-        $user_id = Auth::getAdminId();
+        if($auth) {
+            $user_id = Auth::getAdminId();
+        }
         $controller = Request::controller();
         $action = Request::action();
         $ip = Request::ip();
+        $address = $this->getMethod($ip);
         $url = Request::url();
         $host = Request::host();
         $data = json_encode(Request::post(), JSON_UNESCAPED_UNICODE);
         $params = json_encode(Request::param(), JSON_UNESCAPED_UNICODE);
         $create_time = time();
-        MonitorModel::insert(compact('name','user_id','action','url','data','host','controller','ip','params','create_time'));
+        MonitorModel::insert(compact('name','address','user_id','action','url','data','host','controller','ip','params','create_time'));
+    }
+
+    /**
+     * 获取请求IP省市县
+     */
+    public function getMethod($ip): string
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://searchplugin.csdn.net/api/v1/ip/get?ip='.$ip);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $header[] = 'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $resData = json_decode($response,true);
+        if($resData['code'] == 200) {
+            return $resData['data']['address'];
+        }else {
+            return '未知';
+        }
     }
 
 }
